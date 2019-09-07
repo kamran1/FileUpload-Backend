0ea58bae836a95e870e3dd2aed0b30b412109941
kamran majeed <kamran.student@gmail.com>
2019-09-07 08:21:28 +0500

    First Commit

---
+++ src/main/java/com/kamran/FileuploadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kamran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamran/FileuploadBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamran;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileuploadBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
